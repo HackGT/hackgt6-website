26674fefa05cdd0ff83874904c0fab00299d7b25
Elton <eltonp3103@gmail.com>
2019-06-15 23:02:37 -0400

    added animation for the sun, and on scroll animation for the paths

--- animations.js
+++ animations.js
@@ -1,4 +1,7 @@
 window.addEventListener("load", () => {
+    document.addEventListener('scroll', animatePath1);
+    document.addEventListener('scroll', animatePath2);
+    document.addEventListener('scroll', animatePath3);
     document.addEventListener('scroll', animateHeart);
     document.addEventListener('scroll', animateDiamondBearCard);
     document.addEventListener('scroll', animateEvilQueenCard);
@@ -43,19 +46,54 @@ function animateHeart() {
         element = elementObject.contentDocument.getElementById("clip-rect-heart"),
         elementHeart = elementObject.contentDocument.getElementById("heart");
 
-  if (inViewHeart(element)) {
-      element.classList.add('animateHeart');
+  if (inViewSVG(element, elementObject)) {
       elementHeart.classList.add('display');
+      element.classList.add('animateHeart');
   }
 }
 
-function inViewHeart(element) {
+function animatePath1() {
+    var elementObject = document.getElementById("paths"),
+        element = elementObject.contentDocument.getElementById("clip-rect-path-1"),
+        elementPath = elementObject.contentDocument.getElementById("path-1");
+        console.log(element)
+    if (inViewSVG(element, elementObject)) {
+        element.classList.add('animatePath1');
+        elementPath.classList.add('display');
+    }
+}
+
+function animatePath2() {
+    var elementObject = document.getElementById("paths"),
+        element = elementObject.contentDocument.getElementById("clip-rect-path-2"),
+        elementPath = elementObject.contentDocument.getElementById("path-2");
+        console.log(element)
+    if (inViewSVG(element, elementObject)) {
+        element.classList.add('animatePath2');
+        elementPath.classList.add('display');
+    }
+}
+
+function animatePath3() {
+    var elementObject = document.getElementById("paths"),
+        element = elementObject.contentDocument.getElementById("clip-rect-path-3"),
+        elementPath = elementObject.contentDocument.getElementById("path-3");
+        console.log(element)
+    if (inViewSVG(element, elementObject)) {
+        element.classList.add('animatePath3');
+        elementPath.classList.add('display');
+    }
+}
+
+function inViewSVG(element, elementObject) {
+    console.log(elementObject.clientHeight)
     var windowHeight = window.innerHeight,
         scrollY = window.scrollY,
         scrollPosition = scrollY + windowHeight,
-        elementObjectDiv = document.getElementById('footer-illo-castle'),
-        elementHeight = 0.4 * elementObjectDiv.clientHeight,
-        elementPosition = elementObjectDiv.getBoundingClientRect().top + scrollY + elementHeight;
+        elementHeight = 0.4 * elementObject.clientHeight,
+        elementPosition = elementObject.getBoundingClientRect().top + scrollY + elementHeight;
+    console.log("scroll Position " + scrollPosition)
+    console.log("element Position " + elementPosition)
     return scrollPosition > elementPosition;
 }
 
